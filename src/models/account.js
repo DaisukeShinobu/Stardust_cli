4bae479802ec03ebd9778fbcac8f1fbc05191eb1
DaisukeShinobu <shinobu@ikid.co.jp>
2020-11-12 14:33:52 +0900

    add account model

--- src/models/account.js
+++ src/models/account.js
@@ -2,13 +2,17 @@ import { transformSnakeToCamel } from '../utils/snakeCamelConverter'
 
 export default class Account {
   constructor({
-    id,
-    email,
-    username
-  }) {
+                id,
+                email,
+                birthday,
+                avatar_url,
+                username
+              }) {
     this.id = id
     this.email = email
     this.username = username
+    this.birthday = birthday
+    this.avatar_url = avatar_url
   }
 
   static newFromApiResponse = data => {
